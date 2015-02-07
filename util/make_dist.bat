9a998600d072ee0bcc9ffb31bcf3d9018d0e622e
Gres <gres@gres.biz>
2015-02-07 16:56:34 +0300

    Updated windows distribution script.

--- util/make_dist.bat
+++ util/make_dist.bat
@@ -1,15 +1,15 @@
 @echo off
-set VERSION=3.2.0
+
+set /p VERSION=<..\..\qtcreator-latest\version
 set PLUGIN_NAME=QtcCppcheck
-set BUILD_DIR=c:\Projects\qt-creator\3.2.0\qtcreator-5.3.1-vc10-release
+set BUILD_DIR=..\..\qtcreator-latest\release-vc10
 del %PLUGIN_NAME%-%VERSION%-win.zip
 
 rd /Q /S dist
 mkdir dist\lib\qtcreator\plugins
 mkdir dist\share\qtcreator\translations
 copy /Y %BUILD_DIR%\lib\qtcreator\plugins\%PLUGIN_NAME%.dll        dist\lib\qtcreator\plugins
-copy /Y %BUILD_DIR%\lib\qtcreator\plugins\%PLUGIN_NAME%.pluginspec dist\lib\qtcreator\plugins
-copy /Y ..\translation\*.qm                                             dist\share\qtcreator\translations
+copy /Y ..\translation\*.qm                                        dist\share\qtcreator\translations
 
 cd dist
 zip -q -r ../%PLUGIN_NAME%-%VERSION%-win.zip *
