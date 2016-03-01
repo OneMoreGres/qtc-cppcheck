3af31a358d6f6c775754a1ba525bd7fab97676dc
Francois Ferrand <thetypz@gmail.com>
2016-03-01 09:32:01 +0100

    Fix for QtCreator 4.0

--- src/QtcCppcheckPlugin.cpp
+++ src/QtcCppcheckPlugin.cpp
@@ -48,7 +48,7 @@ namespace
   bool isFileNodeCheckable (const FileNode* node)
   {
     static QStringList extensions = supportedExtensions ();
-    QFileInfo info (node->path().toString());
+    QFileInfo info (node->filePath().toString());
     QString extension = info.completeSuffix ();
     return (extensions.contains (extension));
   }
@@ -251,7 +251,7 @@ QStringList QtcCppcheckPlugin::checkableFiles(const Node *node, bool forceSelect
       const FileNode* file = (const FileNode*) node;
       if (forceSelected || isFileNodeCheckable (file))
       {
-        files << file->path().toString();
+        files << file->filePath().toString();
       }
     }
       break;
