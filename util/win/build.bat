cccf7f67d7edfebd4a5b290ac141a2779505f4cf
Gres <gres@gres.biz>
2017-10-07 19:24:01 +0300

    Added error level checking.

--- util/win/build.bat
+++ util/win/build.bat
@@ -13,6 +13,7 @@ cd build
 lrelease %SELF_PATH%\..\..\qtc-cppcheck.pro
 qmake %SELF_PATH%\..\..
 nmake
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
 
 
@@ -25,6 +26,7 @@ rd /Q /S dist
 mkdir dist\lib\qtcreator\plugins
 mkdir dist\share\qtcreator\translations
 copy /Y qtcreator-latest\compiled\lib\qtcreator\plugins\%PLUGIN_NAME%4.dll dist\lib\qtcreator\plugins
+if %errorlevel% neq 0 exit /b %errorlevel%
 copy /Y %SELF_PATH%\..\..\translation\*.qm dist\share\qtcreator\translations
 
 if exist %PLUGIN_NAME%-%VERSION%-win.zip del /Q %PLUGIN_NAME%-%VERSION%-win.zip
--- util/win/prepare.bat
+++ util/win/prepare.bat
@@ -8,6 +8,7 @@ mkdir qtcreator-latest
 mklink /D qtcreator-latest\Qt %QT_DIR%
 if "%VERSION%"=="" set VERSION=debug
 echo %VERSION%>qtcreator-latest\version
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 
 if exist qtcreator-latest/src goto build
@@ -16,6 +17,7 @@ mkdir download
 if not exist download/src.zip  curl -fsSLk -o download\src.zip http://download.qt.io/official_releases/qtcreator/4.4/4.4.0/qt-creator-opensource-src-4.4.0.zip
 unzip -qq download\src.zip -d qtcreator-latest
 move qtcreator-latest\qt-creator* qtcreator-latest\src
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 
 :build
@@ -26,6 +28,7 @@ mkdir qtcreator-latest\compiled
 cd qtcreator-latest\compiled
 qmake QMAKE_CXXFLAGS+=/MP ..\src
 nmake /S
+if %errorlevel% neq 0 exit /b %errorlevel%
 rd /Q /S src
 cd ..\..
 
