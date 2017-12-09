4a996334294529416f66d657263cf76442f6544a
Gres <gres@gres.biz>
2017-12-09 17:51:42 +0300

    Changed win ci. Not ready.

--- util/win/env.bat
+++ util/win/env.bat
@@ -7,13 +7,13 @@ if /i %ARCH% == x64       goto x64
 goto end
 
 :x64
-call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 set PATH=c:\Qt\5.10\msvc2017_64\bin\;%PATH%
 set QT_DIR=c:\Qt\5.10\msvc2017_64\
 goto end
 
 :x86
-call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 set PATH=c:\Qt\5.10\msvc2017\bin\;%PATH%
 set QT_DIR=c:\Qt\5.10\msvc2017\
 goto end
